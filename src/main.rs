37f8a90a5f1565d80c299a7e522a805d99ce10c2
isaac-asdf <97187398+isaac-asdf@users.noreply.github.com>
2024-12-21 07:53:30 -0600

    code cleanup w/ config

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,81 @@
 use futures::future::join_all;
 use nostr_sdk::prelude::*;
+use serde::{Deserialize, Serialize};
 use std::io::Read;
 use std::sync::mpsc::{channel, Receiver};
 use std::thread;
+use toml;
+
+const CONFIGSTR: &str = "./Config.toml";
+const DVM_REQ: Kind = Kind::JobRequest(5300);
+const DVM_RESP: Kind = Kind::JobResult(6300);
+const DVM_ADVERT: Kind = Kind::Custom(31990);
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Config {
+    package: Package,
+    comms: Comms,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Package {
+    about: String,
+    lnurl: String,
+    nsec: Option<String>,
+    announced: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Comms {
+    relays: Vec<String>,
+    admins: Vec<String>,
+    npubs: Vec<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // get keys (generate first time with Keys::generate())
-    let mut f = std::fs::File::open("./keys.txt").unwrap();
-    let mut keys = String::new();
-    f.read_to_string(&mut keys).unwrap();
-    let keys = Keys::parse(&keys).unwrap();
+    // get config
+    let config = get_config();
+    let keys: Keys = Keys::parse(&config.package.nsec.expect("nsec generation error")).unwrap();
+    let npubs = get_npubs(&config.comms.npubs);
+    let admins = get_admins(&config.comms.npubs);
     let client = Client::new(keys.clone());
-    // let client = Client::default();
-
-    add_relays(&client).await.unwrap();
-    // Connect to relays
+    add_relays(&client, &config.comms.relays).await.unwrap();
     client.connect().await;
 
-    let (sender, receiver) = channel::<Event>();
+    // verify we have announced
+    if !config.package.announced {}
 
-    let npubs = get_npubs();
+    println!("Find me at: {}", keys.public_key().to_bech32()?);
+
+    // Generate filters for subscriptions
     let sub_notes = Filter::new()
         .authors(npubs)
         .kind(Kind::TextNote)
         .since(Timestamp::min());
+    let sub_dvmreq = Filter::new().kind(DVM_REQ).since(Timestamp::now());
+    // todo: Verify DM decrypting
+    // let sub_admin = Filter::new()
+    //     .authors(admins.clone())
+    //     .kinds(vec![
+    //         Kind::PrivateDirectMessage,
+    //         Kind::EncryptedDirectMessage,
+    //     ])
+    //     .since(Timestamp::min());
+    let Output { .. } = client.subscribe(vec![sub_notes, sub_dvmreq], None).await?;
 
-    let admins = get_admins();
-    let _sub_admin = Filter::new() //     .authors(admins.clone())
-        .kinds(vec![
-            Kind::PrivateDirectMessage,
-            Kind::EncryptedDirectMessage,
-        ])
-        .since(Timestamp::now());
-
-    // Subscribe (auto generate subscription ID)
-    let Output { .. } = client.subscribe(vec![sub_notes], None).await?;
-
-    // Spawn off an expensive computation
+    // Setup background thread for processing received events
+    let (sender, receiver) = channel::<Event>();
+    let c = client.clone();
     thread::spawn(move || {
-        handle_events(receiver, admins);
+        handle_events(receiver, admins, c);
     });
 
-    // Handle subscription notifications with `handle_notifications` method
+    // Wait for events to come in
     client
         .handle_notifications(|notification| async {
             if let RelayPoolNotification::Event { event, .. } = notification {
-                if event.kind == Kind::TextNote {
-                    sender.send(*event).unwrap();
-                } else if event.kind == Kind::PrivateDirectMessage {
-                    sender.send(*event).unwrap();
-                } else if event.kind == Kind::EncryptedDirectMessage {
-                    sender.send(*event).unwrap();
-                }
+                sender.send(*event).unwrap();
             }
             Ok(false) // Set to true to exit from the loop
         })
@@ -62,35 +85,87 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-fn handle_events(_receiver: Receiver<Event>, _admins: Vec<PublicKey>) {
-    // todo
+/// Shuffle received events to proper function.
+/// Update event list with TextNotes, Send DVM_RESP when we get a REQ,
+/// and handle admin DMs
+fn handle_events(receiver: Receiver<Event>, _admins: Vec<PublicKey>, client: Client) {
     println!("Waiting for events");
-    loop {}
+    let mut events: Vec<Event> = Vec::new();
+    loop {
+        let ev = receiver.recv().unwrap();
+        match ev.kind {
+            Kind::TextNote => update_event_list(&mut events, ev, &client),
+            Kind::Custom(5300) => send_resp(ev, &client),
+            Kind::PrivateDirectMessage => handle_cmd(ev, &client),
+            Kind::EncryptedDirectMessage => handle_cmd(ev, &client),
+            _ => (),
+        }
+    }
 }
 
-async fn add_relays(client: &Client) -> Result<()> {
-    let mut f = std::fs::File::open("./relays.txt").unwrap();
-    let mut relays = String::new();
-    f.read_to_string(&mut relays).unwrap();
-    let forjoin = relays.split("\n").map(|r| client.add_relay(r));
+/// Send a 6300 after getting a 5300, referencing the request job
+fn send_resp(event: Event, client: &Client) {
+    // reference event ID e
+    // reference pubkey p
+    // status: success
+    // alt: result of DVM
+    // relays: relays used
+    // content: list of [e, [eid]]
+}
+
+/// Adds an event to the list if it is not present yet.
+/// Uses a simple time based ordering
+fn update_event_list(list: &mut Vec<Event>, event: Event, client: &Client) {
+    if !list.contains(&event) {
+        list.push(event);
+        list.sort_by(|a, b| {
+            if a.created_at == b.created_at {
+                a.id.cmp(&b.id)
+            } else {
+                a.created_at.cmp(&b.created_at)
+            }
+        });
+        while list.len() > 200 {
+            list.pop();
+        }
+    }
+}
+
+fn handle_cmd(_event: Event, _client: &Client) {
+    // unimplemented
+}
+
+fn get_config() -> Config {
+    let mut f = std::fs::File::open(CONFIGSTR).unwrap();
+    let mut filestr = String::new();
+    f.read_to_string(&mut filestr).unwrap();
+    let mut config: Config = toml::from_str(&filestr).unwrap();
+    if config.package.nsec.is_none() {
+        let keys = Keys::generate();
+        config.package.nsec = Some(keys.secret_key().to_secret_hex());
+        save_config(&config);
+    }
+    config
+}
+fn save_config(config: &Config) {
+    let out = toml::to_string_pretty(config).unwrap();
+    std::fs::write(CONFIGSTR, &out).unwrap();
+}
+async fn add_relays(client: &Client, relays: &Vec<String>) -> Result<()> {
+    let forjoin = relays.iter().map(|r| client.add_relay(r));
     join_all(forjoin).await;
 
     Ok(())
 }
-
-fn get_npubs() -> Vec<PublicKey> {
-    let mut f = std::fs::File::open("./npubs.txt").unwrap();
-    let mut keys = String::new();
-    f.read_to_string(&mut keys).unwrap();
-    keys.split("\n")
+fn get_npubs(npubs: &Vec<String>) -> Vec<PublicKey> {
+    npubs
+        .iter()
         .map(|pubkey| PublicKey::parse(pubkey).unwrap())
         .collect::<Vec<PublicKey>>()
 }
-fn get_admins() -> Vec<PublicKey> {
-    let mut f = std::fs::File::open("./admins.txt").unwrap();
-    let mut keys = String::new();
-    f.read_to_string(&mut keys).unwrap();
-    keys.split("\n")
+fn get_admins(admins: &Vec<String>) -> Vec<PublicKey> {
+    admins
+        .iter()
         .map(|pubkey| PublicKey::parse(pubkey).unwrap())
         .collect::<Vec<PublicKey>>()
 }
